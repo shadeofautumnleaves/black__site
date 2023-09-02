1abfbb83c4fb56764438defd9afb5b43af2481bf
shadeofautumnleaves <143270052+shadeofautumnleaves@users.noreply.github.com>
2023-09-02 17:39:57 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.querySelector(".burger-lines")
+    .addEventListener("click", () => {
+        document.querySelector(".burger-menu")
+
+            .classList.toggle("active")
+            console.log('work');
+    })
+
